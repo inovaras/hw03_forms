32629c693ae4b8c1ed76808bb4e89866ea5991e4
Albina Gilmutdinova <inovaras@mail.ru>
2023-03-02 15:35:20 +0500

    code according to PEP8

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -77,6 +77,10 @@ def post_edit(request, post_id):
         if form.is_valid():
             form.save()
             return redirect('posts:post_detail', post_id=post_id)
-        return render(request, 'posts/create.html', {'form': form, 'is_edit': True})
+        return render(
+            request, 'posts/create.html', {'form': form, 'is_edit': True}
+        )
     form = PostForm(instance=post)
-    return render(request, 'posts/create.html', {'form': form, 'is_edit': True})
+    return render(
+        request, 'posts/create.html', {'form': form, 'is_edit': True}
+    )
