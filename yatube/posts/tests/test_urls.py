0d2b8d172b7825d1b5d24ad7d78384accd811593
Albina Gilmutdinova <inovaras@mail.ru>
2023-03-06 16:23:43 +0500

    start posts_urls test

--- yatube/posts/tests/test_urls.py
+++ yatube/posts/tests/test_urls.py
@@ -1,9 +1,27 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase, Client
 
+from ..models import Post
 
-class StaticURLTests(TestCase):
+User = get_user_model()
+
+
+class PostURLTests(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        Post.objects.create(
+            text='Проверочный текст',
+            pub_date='01.01.01',
+            author='test_author',
+            group='test_group'
+        )
+
     def setUp(self):
         self.guest_client = Client()
+        self.user = User.objects.create_user(username='HasNoName')
+        self.authorized_client = Client()
+        self.authorized_client.force_login(self.user)
 
     def test_homepage(self):
         response = self.guest_client.get('/')
